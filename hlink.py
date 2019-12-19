a50db988dd992c8ee56c024ccad397fb55c2fde7
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2019-12-19 20:41:34 +0900

    サーチ→オープンができるようになった

--- hlink.py
+++ hlink.py
@@ -9,6 +9,7 @@ import os
 import sys
 import json
 import sqlite3
+import logging
 
 
 class SQL:
@@ -25,6 +26,12 @@ insert into url(key, url) values (
   ?, ?
 );
 """
+    @staticmethod
+    def search_by_key(key):
+        return f"""
+select * from url
+where key like '{key}';
+"""
 
 
 class DB:
@@ -35,6 +42,7 @@ class DB:
         self.path = path
         self.check_db()
         self.con = sqlite3.connect(self.path)
+        self.con.row_factory = sqlite3.Row
 
     def check_db(self):
         if not os.path.exists(self.path):
@@ -55,21 +63,18 @@ create one ? [y/n]
             con.commit()
 
     def execute(self, sql):
+        logging.debug(sql)
         cur = self.con.cursor()
-        try:
-            cur.execute(sql)
-            self.con.commit()
-        except Exception as e:
-            self.con.rollback()
-            raise e
-        finally:
-            cur.close()
+        rows = cur.execute(sql)
+        return [row for row in rows]
 
     def executemany(self, sql, it):
+        logging.debug(sql)
         cur = self.con.cursor()
         try:
-            cur.executemany(sql, it)
+            ret = cur.executemany(sql, it)
             self.con.commit()
+            return ret
         except Exception as e:
             self.con.rollback()
             raise e
@@ -77,10 +82,12 @@ create one ? [y/n]
             cur.close()
 
     def executescript(self, sql):
+        logging.debug(sql)
         cur = self.con.cursor()
         try:
-            cur.executescript(sql)
+            ret = cur.executescript(sql)
             self.con.commit()
+            return ret
         except Exception as e:
             self.con.rollback()
             raise e
@@ -95,8 +102,15 @@ class Repository:
         self.db = DB(db_path)
 
     def add(self, key, url):
-        self.db.executemany(SQL.insert_url,
-                            [(key, url)])
+        return self.db.executemany(SQL.insert_url,
+                                   [(key, url)])
+
+    def search(self, keys):
+        like = "%" + "%".join(keys) + "%"
+        ret = self.db.execute(SQL.search_by_key(like))
+        return [dict(id=row["id"],
+                     key=row["key"],
+                     url=row["url"]) for row in ret]
 
 
 class Command:
@@ -113,7 +127,7 @@ class Command:
 
 class Add(Command):
     "add url"
-    name = "add"
+    name = "a"
 
     def __init__(self, subparsers):
         super(Add, self).__init__(subparsers)
@@ -128,18 +142,53 @@ class Add(Command):
         print(num)
 
 
+class Search(Command):
+    "search by key"
+    name = "s"
+
+    def __init__(self, subparsers):
+        super(Search, self).__init__(subparsers)
+
+    def register_argument(self):
+        self.parser.add_argument("keys", nargs="*")
+
+    def handler(self, args, repo):
+        vals = repo.search(args.keys)
+        if len(vals) == 0:
+            sys.stderr.write("no match\n")
+        elif len(vals) == 1:
+            url = vals[0]["url"]
+            App.open(url)
+        else:
+            for v in vals:
+                print("{0[id]}: {0[key]}, {0[url]}".format(v))
+
+
 class App:
     repository = None
     conf_dir = os.path.expanduser("~/.hlink")
     conf_path = os.path.join(conf_dir, "config.json")
     conf = dict(
-        db_path=os.path.join(conf_dir, "db.sqlite")
+        db_path=os.path.join(conf_dir, "db.sqlite"),
+        log_path="/tmp/hlink.log"
     )
 
     def __init__(self):
         self.check_conf_dir()
         self.read_config()
         self.repository = Repository(self.db_path())
+        self.setup_logger()
+
+    def setup_logger(self):
+        logging.basicConfig(
+            format="[%(levelname)s]%(asctime)s %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+            level=logging.DEBUG,
+            filename=self.log_path()
+        )
+
+    def log_path(self):
+        return self.conf["log_path"]
 
     def db_path(self):
         return self.conf["db_path"]
@@ -161,7 +210,7 @@ create one ? [y/n]
         os.mkdir(self.conf_dir)
 
     def run(self):
-        commands = [Add]
+        commands = [Add, Search]
 
         parser = argparse.ArgumentParser(description="sample")
         subparsers = parser.add_subparsers()
@@ -182,6 +231,10 @@ create one ? [y/n]
         else:
             with open(self.conf_path, "w") as fp:
                 json.dump(self.conf, fp)
+
+    @staticmethod
+    def open(file):
+        os.system(f"open {file}")
 
 
 if __name__ == "__main__":
